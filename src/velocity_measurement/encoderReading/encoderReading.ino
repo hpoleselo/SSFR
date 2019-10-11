06df6589fd85796fd8db39b32a648e29151bf872
Henrique Poleselo <hpoleselo@gmail.com>
2019-10-11 17:58:01 -0300

    adapted code to measure linear velocity in km/h from another source code

--- src/velocity_measurement/encoderReading/encoderReading.ino
+++ src/velocity_measurement/encoderReading/encoderReading.ino
@@ -1,44 +1,39 @@
-    // Encoder LM393 program
-    int D0IN = 2;
-    int rpm;
-    volatile byte pulses;
-    unsigned long timeold;
-    // How many holes one complete rotation in our encoder has
-    unsigned int pulsesPerRound = 20;
-    void counter()
-    {
-      // Everytime we detect a hole it counts as one pulse, pulses = pulses + 1
-      pulses++;
-    }
-    void setup()
-    {
-      Serial.begin(9600);
-      // The pin has be our input since we're reading the pulses from the sensor
-      pinMode(D0IN, INPUT);
-      //Interrupcao 0 - pino digital 2
-      // Everytime the interrupt is triggered (borda de descida) we call the counter
-      attachInterrupt(0, counter, FALLING);
-      pulses = 0;
-      rpm = 0;
-      timeold = 0;
-    }
-    void loop()
-    {
-      // ACHO QUE DEVEMOS DIIMINUIR PRA SER A CADA MILISEGUNDO MESMO E IR SALVANDO (DIMINUIR PRA 10 OU 1)
-      // Updates the counter each second
-      // millis() returns the number of milliseconds passed since the Arduino board began running the current program
-      if (millis() - timeold >= 1000)
-      {
-        //Desabilita interrupcao durante o calculo
-        detachInterrupt(0);
-        // TALVEZ MUDAR A FORMULA
-        rpm = (60 * 1000 / pulsesPerRound ) / (millis() - timeold) * pulses;
-        timeold = millis();
-        pulses = 0;
-        // Shows the angular velocity in the serial monitor
-        Serial.print("RPM = ");
-        Serial.println(rpm, DEC);
-        //Habilita interrupcao
-        attachInterrupt(0, counter  , FALLING);
-      }
-    }
+unsigned long start;
+const byte encoderPinA = 3;//A pin -> interrupt pin 0
+const byte encoderPinB = 4;//B pin -> digital pin 4
+volatile long pulse;
+volatile bool pinB, pinA, dir;
+const byte ppr = 20, upDatesPerSec = 2;
+const int fin = 1000 / upDatesPerSec;
+const float konstant = 60.0 * upDatesPerSec / (ppr * 2);
+int rpm;
+double v;
+
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(1, readEncoder, CHANGE);
+  pinMode(encoderPinA,INPUT_PULLUP);
+  pinMode(encoderPinB,INPUT_PULLUP);
+}
+
+void loop() {
+  if(millis() - start > fin)
+  {
+    start = millis();
+    rpm = pulse * konstant;
+    // convert to linear velocity in km/h considering a wheel of diameter 0.0325m
+    v= 2*3.1415*(rpm/60.0)*0.0325*3.6;
+    
+    Serial.println(v,7);
+    pulse = 0;
+  }
+}
+
+void readEncoder()
+{
+  pinA = bitRead(PIND,encoderPinA);
+  pinB = bitRead(PIND,encoderPinB);
+  dir = pinA ^ pinB;          // if pinA & pinB are the same
+  dir ? --pulse : ++pulse;    // dir is CW, else CCW
+}
