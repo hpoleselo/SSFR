b79a232fce82c2990fe41da04fe9d92b2a09f852
Henrique Poleselo <hpoleselo@gmail.com>
2019-11-10 20:07:42 -0300

    Update

--- PlantModel/WheelModel/plantModelForAngularVelocity.m
+++ PlantModel/WheelModel/plantModelForAngularVelocity.m
@@ -1,33 +1,40 @@
 % Reading our filtered data
-[t, phi1] = readAndFilterData();
+[t, w] = readNFilterData();
 
-n = length(phi1);     
+n = length(w);     
 
 % Reconstruct system response w/ 30/255 from duty cycle
-plot(t,phi1,'r')
+plot(t,w,'r')
 grid on
 
+%input vel 56 rad/s for 27% from the PWM
+input = 70;                        % rad/s got from the graph from readNFilterData
+
 % Three parameter model approximation G3(s) = e^-Ls*k/Ts+1
-%y_infinite = 0.35;          % Steady state value, we checked from the grph
-%k = y_infinite/54;           % 0.0065, 54 is the input velocity of the wheel
-L = 10.303-10.180;           % 0.123s
+y_infinite = 55.29;                % Steady state value, we checked from the grph
+k = y_infinite/input;
+L = 0.2540 -0.1010;                % 0.153s
 
 % Helping function to calculate the Ao, where T2 = Ao/y_inf - L
-%hold on
-%const = 0.35*ones(1,n);      % Steady state of our system
-%plot(t,const,'k')
+hold on
+const = y_infinite*ones(1,n);      % Steady state of our system
+plot(t,const,'k')
+
+resultant = const - w;           % Area between both curves
+lowerLimit = 0.1010;             % Where the curves start
+% When  the system reaches the steady state
+upperLimit = 0.97;               % Was chosen with ginput(1) on the graph
+tForIntegration = linspace(lowerLimit,upperLimit,n);
+Ao = trapz(tForIntegration,resultant);
+T1 = Ao/y_infinite - L;
 
 
-%resultant = const - omega;   % Area between both curves
-lowerLimit = 10.180;         % Where the curves start
-upperLimit = 10.9506;        % Was chosen with ginput(1) on the graph
-%tForIntegration = linspace(lowerLimit,upperLimit,n);
-%Ao = trapz(tForIntegration,resultant);
-%T1 = Ao/y_infinite - L; % checar area, depois q mudei a escala de t, mudou
-
+% Model 3 parameters without the area method
 % Another approximation for the time response T (tAB 63% from the y(inf))
 % 0.63*y(inf) = 0.2205, then going to the graph with ginput(1) see which
 % value we have for the time
+time_cte = 0.63*y_infinite;
+    
 T2 = 10.433 - 10.303;        % 10.433 foi pegando pelo ginput e 10.303 eh o atraso
 
 
--- PlantModel/WheelModel/readNFilterData.m
+++ PlantModel/WheelModel/readNFilterData.m
@@ -1,36 +1,43 @@
-function [t1, phi1_filtered] = readNFilterData()
+function [t1, w_filtered] = readNFilterData()
     % Order of the moving averaging filter
     n = 5;
 
     % Read CSV data
-    S_matrix = readtable('angularVelCurve.csv');    % System response
+    S_matrix = readtable('systemVelCurve.csv');    % System response
     I_matrix = readtable('inputVelCurve.csv');      % Input
-    
 
     % Separate the data in vectors and tranposes
-    time = table2array(S_matrix(:,1)).';   % gets the time vector
-    phi1 = table2array(S_matrix(:,2)).';   % gets the phi1 vector
+    time1 = table2array(S_matrix(:,1)).';   % gets the time vector
+    w = table2array(S_matrix(:,2)).';   % gets the phi1 vector
     
     % Doing the same but for the input of our system
     time2 = table2array(I_matrix(:,1)).';
-    phi2 = table2array(I_matrix(:,2)).';
+    w2 = table2array(I_matrix(:,2)).';
     
     % Converting from miliseconds to seconds
-    t1 = 0.001*time;
-    t2 = 0.001*time;
+    t1 = 0.001*time1;
+    t2 = 0.001*time2;
     
-    % Uncomment to check the original graph without filtering
-    %plot(t,phi1,'b');
-
-    for i = n:length(phi1)
-       phi1_filtered(i) = (phi1(i)+phi1(i-(n-4))+phi1(i-(n-3))+phi1(i-(n-2))+phi1(i-(n-1)))/n; 
+    for i = n:length(w)
+       w_filtered(i) = (w(i)+w(i-(n-4))+w(i-(n-3))+w(i-(n-2))+w(i-(n-1)))/n; 
+    end
+    
+    % Filtering the input to check what is the velocity
+    for i = n:length(w2)
+       w2_filtered(i) = (w2(i)+w2(i-(n-4))+w2(i-(n-3))+w2(i-(n-2))+w2(i-(n-1)))/n; 
     end
     
     % Uncomment to check both filtered and unfiltered
     %hold on
-    %plot(time, phi1_filtered,'r');
+    %plot(time, theta_filtered,'r');
     %grid on
 
+    % Comparing the input graph, before and after filtering: (uncomment)
+    %plot(t2,w2,'b');
+    %hold on
+    %plot(t2,w2_filtered,'r');
+    
+    
     % Another filtration if needed
     %for i = n:length(phi1_filtered)
     %   phi1_filtered1(i) = (phi1_filtered(i)+phi1_filtered(i-(n-4))+phi1_filtered(i-(n-3))+phi1_filtered(i-(n-2))+phi1_filtered(i-(n-1)))/n; 
@@ -38,4 +45,3 @@ function [t1, phi1_filtered] = readNFilterData()
     %hold on
     %plot(time,phi1_filtered1,'k');
 end
-
